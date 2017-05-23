a0c386ced16df8b708cb0bfd876d44d9048d4a40
Revega Denis <rden82@users.noreply.github.com>
2017-05-23 22:41:00 +0300

    TeamCity
    Project

---
+++ getPagesSource.js
@@ -0,0 +1,34 @@
+function DOMtoString(document_root) {
+    var html = '';
+    var node = document_root.firstChild;
+    var url = document_root.location.href;	
+    while (node) {
+        switch (node.nodeType) {
+            case Node.ELEMENT_NODE:
+                html += node.outerHTML;
+                break;
+            case Node.TEXT_NODE:
+                html += node.nodeValue;
+                break;
+            case Node.CDATA_SECTION_NODE:
+                html += '<![CDATA[' + node.nodeValue + ']]>';
+                break;
+            case Node.COMMENT_NODE:
+                html += '<!--' + node.nodeValue + '-->';
+                break;
+            case Node.DOCUMENT_TYPE_NODE:
+                // (X)HTML documents are identified by public identifiers
+                html += "<!DOCTYPE " + node.name + (node.publicId ? ' PUBLIC "' + node.publicId + '"' : '') + (!node.publicId && node.systemId ? ' SYSTEM' : '') + (node.systemId ? ' "' + node.systemId + '"' : '') + '>\n';
+                break;
+        }
+        node = node.nextSibling;
+    }
+	var htmlAr = [html, url];
+
+    return htmlAr;
+}
+
+chrome.runtime.sendMessage({
+    action: "getSource",
+    source: DOMtoString(document)
+});
---
+++ popup.js
@@ -0,0 +1,81 @@
+﻿chrome.runtime.onMessage.addListener(function(request, sender) {
+  function copyTextToClipboard(text) {
+    var textArea = document.createElement("textarea");
+    
+    textArea.style.position = 'fixed';
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+    textArea.style.width = '2em';
+    textArea.style.height = '2em';
+    textArea.style.padding = 0;
+    textArea.style.border = 'none';
+    textArea.style.outline = 'none';
+    textArea.style.boxShadow = 'none';
+    textArea.style.background = 'transparent';
+    textArea.value = text;
+
+    document.body.appendChild(textArea);
+
+    textArea.select();
+
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Copying text command was ' + msg);
+    } catch (err) {
+      console.log('Oops, unable to copy');
+    }
+
+    document.body.removeChild(textArea);
+  }
+  
+  if (request.action == "getSource") {
+    var S = request.source;
+    
+    var Location = S[1];
+    var $html = $(S[0]);
+    var Branch = $html.find('button.btn.btn_mini').attr('onclick').split('branchName: \'')[1].split('\' }')[0].replace("\\","");
+    
+    if (Branch == '<default>') {
+      Branch = 'master';
+    }
+	
+	  var Channel = $html.find('.last.buildType').text();
+	  var Assembly = $html.find('.contentWrapper').text().split( /#/ )[1];
+
+	  var CSSVar = 'background-color: black; color: lime; font-weight: lighter; font-size: 16px;';
+      var OS;
+	  
+	  switch (Channel.split('.').pop(-1)) {
+		  case "win" : OS = "__Win7 x64__"; 
+			break;
+		  case "mac" : OS = "__MacOS 10.12__"; 
+			break;
+		  default: OS = "__Ubuntu 17.04__";		  
+	  }
+	
+	var Result = OS + ": ветка %%" + Branch + ", Branded distributives > " + Channel + " >%% ((" + Location + " " + Assembly + " ))";
+    var info = '<span style="font-weight: bold; font-size: 16px; color: white;">The following link copied into clipboard:</span><br>';
+    var Str = '<span style="' + CSSVar + '">' + Result + '</span>';
+
+    copyTextToClipboard(Result);
+
+    message.innerHTML = info + Str;
+  }
+});
+
+function onWindowLoad() {
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
+}
+
+window.onload = onWindowLoad;
